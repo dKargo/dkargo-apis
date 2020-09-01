49146363ca3708171846fc089c4814e6dc5ab266
hlib <hlib.master@gmail.com>
2020-09-01 10:50:46 +0900

    [doc: systemlog 수정]
    - log 문구 수정함 (stop.sh)

--- services/stop.sh
+++ services/stop.sh
@@ -2,5 +2,5 @@
 FILE_PATH=`dirname "$0"`
 LOG_PATH=$FILE_PATH/..
 timestamp=`date +%Y/%m/%d-%H:%M`
-echo "[$timestamp]: [EXPLORER]: npm stop..." >> "$LOG_PATH/systemlog"
+echo "[$timestamp]: [APIS]: npm stop..." >> "$LOG_PATH/systemlog"
 npm stop &
