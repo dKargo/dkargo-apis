90b2404a5ba9c211c1d01297ed72d4eac8bce72e
hlib <hlib.master@gmail.com>
2020-09-08 11:13:17 +0900

    [feature: 변경된 컨트랙트 ABI 반영]
    - 변경된 컨트랙트 ABI 반영
        : build/contracts
    - hexa to ascii lib function 추가
        : libs/libCommon.js

--- libs/libCommon.js
+++ libs/libCommon.js
@@ -1,8 +1,9 @@
 /**
  * @file libCommon.js
- * @notice 유용한 lib Functions 정의
+ * @notice 유용한 lib Functions 및 constant 정의
  * @author jhhong
  */
+const Log = require('./libLog.js').Log; // 로그 출력
 
 /**
  * @notice ms만큼 딜레이를 수행한다.
@@ -13,3 +14,32 @@
 module.exports.delay = function(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+/**
+ * @notice 16진수(HEX)을 ASCII로 반환한다.
+ * @param hex 16진수(HEX) 문자열
+ * @author jhhong
+ */
+module.exports.hex2a = async function(hex) {
+    try {
+        if(hex == undefined || hex == null || hex.length == 0) {
+            throw new Error(`Invalid Hex String!`);
+        }
+        let str = '';
+        for (let i = 0; i < hex.length; i += 2) {
+            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+        }
+        return str;
+    } catch(error) {
+        let action = `Action: hex2a
+        - [hex]: [${hex}]`;
+        Log('ERROR', `exception occured!:\n${action}\n${colors.red(error.stack)}`);
+        return null;
+    }
+}
+
+/**
+ * @notice ZERO ADDRESS 정의
+ * @author jhhong
+ */
+module.exports.ZEROADDR = '0x0000000000000000000000000000000000000000';
